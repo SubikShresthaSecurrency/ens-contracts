085b04d1071ec7db89b7dac401cacff0b0720a2f
SubikShresthaSecurrency <subik.shrestha@securrency.com>
2025-03-20 14:33:37 +0545

    chore: deployments

--- scripts/.js
+++ scripts/.js
@@ -32,93 +32,105 @@ const batchData = [
     name: 'dtcc',
     subdomains: [
       {
-        name: 'margincore-ny',
-        address: '0x14D6F2201FC8c8cfd65Dd349dD53d165198C22f7',
+        name: 'mr-nametwo',
+        address: '0x28c340B6EB03C80548c50731391FF60318B3c809',
       },
-      {
-        name: 'margincore-tokyo',
-        address: '0x803B307bbEcB8D8E55959C0F21cD6a5E008D58Ef',
-      },
-      {
-        name: 'margincore-paris',
-        address: '0x81f845B586fFf45C68F01D2B4d9c0DB727a8cE3d',
-      },
-      {
-        name: 'bravo-bank',
-        address: '0x88Cf1538EEB86462C2AC66702A80cC6a8EC4fD04',
-      },
-      {
-        name: 'socgen',
-        address: '0x93f0BC752C988aB66DEA14dD8Aa6176b2B511381',
-      },
-      {
-        name: 'wellington',
-        address: '0xaEbA76b426fEE6680872a84942fda1bC4D1A87Ef',
-      },
-      {
-        name: 'bank-of-america',
-        address: '0x6774f079Dd360cf228d09737BC3657B8672bABBB',
-      },
-      {
-        name: 'sterling-bank',
-        address: '0xfABB6B1528B83D91941c3CB58e7954522c24d8CA',
-      },
-      {
-        name: 'fidelity',
-        address: '0xce1279683e3A060Df76E72f0D1717CF95D42Be9b',
-      },
-      {
-        name: 'ficc',
-        address: '0x37a7F8B45f7dCCC4d88eCf8B8866F056a512395F',
-      },
-      {
-        name: 'nscc',
-        address: '0x801839DebB7061406EF738d507D05830F2fF43f1',
-      },
-      {
-        name: 'jscc',
-        address: '0x01d2D3da7a42F64e7Dc6Ae405F169836556adC86',
-      },
-      {
-        name: 'crypto-ex1',
-        address: '0x57122554635EF92adC3c9252Ea996ce9Fb72537C',
-      },
-      {
-        name: 'dtcc-da',
-        address: '0xc31a19c17981b6ACd925Af8c8F5BF7a63aeAA028',
-      },
-      {
-        name: 'dtcc',
-        address: '0x9300787cFe46c0468cca4aa1b439fa627FAa28C7',
-      },
-      {
-        name: 'bny',
-        address: '0xF466381A18b44Ef898B3e95d616A8Cae53a560D0',
-      },
-      {
-        name: 'euroclear',
-        address: '0x1BE6Fe727aCD01f86ab5b5C7b57d42152c4CB771',
-      },
-      {
-        name: 'blackrock',
-        address: '0xAF0AD51DaD551833495cD679344D71fB6990d3a2',
-      },
-      {
-        name: 'crypto-ex2',
-        address: '0xfaB5663a0d7F277F926381B5B5b1C2078288700E',
-      },
-      {
-        name: 'jp-morgan',
-        address: '0x4ea3efe0207a9A208C0da93A3f9A7A898393fe4D',
-      },
-      {
-        name: 'margincore-london',
-        address: '0x9d1aE6d84e3A385e667CeE70d4dFe29Ff0ea7605',
-      },
-      {
-        name: 'yan-asset-manager',
-        address: '0xbB22D30cF7075339FbbC12035AFBd8aa23C7f355',
-      },
+      // {
+      //   name: 'margincore-tokyo',
+      //   address: '0x803B307bbEcB8D8E55959C0F21cD6a5E008D58Ef',
+      // },
+      // {
+      //   name: 'zach',
+      //   address: '0x5eb3Bc0a489C5A8288765d2336659EbCA68FCd00',
+      // },
+      // {
+      //   name: 'margincore-ny',
+      //   address: '0x14D6F2201FC8c8cfd65Dd349dD53d165198C22f7',
+      // },
+      // {
+      //   name: 'margincore-tokyo',
+      //   address: '0x803B307bbEcB8D8E55959C0F21cD6a5E008D58Ef',
+      // },
+      // {
+      //   name: 'margincore-paris',
+      //   address: '0x81f845B586fFf45C68F01D2B4d9c0DB727a8cE3d',
+      // },
+      // {
+      //   name: 'bravo-bank',
+      //   address: '0x88Cf1538EEB86462C2AC66702A80cC6a8EC4fD04',
+      // },
+      // {
+      //   name: 'socgen',
+      //   address: '0x93f0BC752C988aB66DEA14dD8Aa6176b2B511381',
+      // },
+      // {
+      //   name: 'wellington',
+      //   address: '0xaEbA76b426fEE6680872a84942fda1bC4D1A87Ef',
+      // },
+      // {
+      //   name: 'bank-of-america',
+      //   address: '0x6774f079Dd360cf228d09737BC3657B8672bABBB',
+      // },
+      // {
+      //   name: 'sterling-bank',
+      //   address: '0xfABB6B1528B83D91941c3CB58e7954522c24d8CA',
+      // },
+      // {
+      //   name: 'fidelity',
+      //   address: '0xce1279683e3A060Df76E72f0D1717CF95D42Be9b',
+      // },
+      // {
+      //   name: 'ficc',
+      //   address: '0x37a7F8B45f7dCCC4d88eCf8B8866F056a512395F',
+      // },
+      // {
+      //   name: 'nscc',
+      //   address: '0x801839DebB7061406EF738d507D05830F2fF43f1',
+      // },
+      // {
+      //   name: 'jscc',
+      //   address: '0x01d2D3da7a42F64e7Dc6Ae405F169836556adC86',
+      // },
+      // {
+      //   name: 'crypto-ex1',
+      //   address: '0x57122554635EF92adC3c9252Ea996ce9Fb72537C',
+      // },
+      // {
+      //   name: 'dtcc-da',
+      //   address: '0xc31a19c17981b6ACd925Af8c8F5BF7a63aeAA028',
+      // },
+      // {
+      //   name: 'dtcc',
+      //   address: '0x9300787cFe46c0468cca4aa1b439fa627FAa28C7',
+      // },
+      // {
+      //   name: 'bny',
+      //   address: '0xF466381A18b44Ef898B3e95d616A8Cae53a560D0',
+      // },
+      // {
+      //   name: 'euroclear',
+      //   address: '0x1BE6Fe727aCD01f86ab5b5C7b57d42152c4CB771',
+      // },
+      // {
+      //   name: 'blackrock',
+      //   address: '0xAF0AD51DaD551833495cD679344D71fB6990d3a2',
+      // },
+      // {
+      //   name: 'crypto-ex2',
+      //   address: '0xfaB5663a0d7F277F926381B5B5b1C2078288700E',
+      // },
+      // {
+      //   name: 'jp-morgan',
+      //   address: '0x4ea3efe0207a9A208C0da93A3f9A7A898393fe4D',
+      // },
+      // {
+      //   name: 'margincore-london',
+      //   address: '0x9d1aE6d84e3A385e667CeE70d4dFe29Ff0ea7605',
+      // },
+      // {
+      //   name: 'yan-asset-manager',
+      //   address: '0xbB22D30cF7075339FbbC12035AFBd8aa23C7f355',
+      // },
     ],
   },
 ]
@@ -148,10 +160,6 @@ async function main() {
 
     try {
       for (const subdomain of entry.subdomains) {
-        const subdomainNamehash = utils.namehash(
-          `${subdomain.name}.${entry.name}.${tld}`,
-        )
-
         console.log(`Setting subdomain ${subdomain.name}.${entry.name}.${tld}`)
 
         console.log(
@@ -160,11 +168,10 @@ async function main() {
 
         console.log(deployer.address)
 
-        tx = await ENSRegistry.connect(deployer).setSubnodeOwner(
+        tx = await ENSRegistry.setSubnodeOwner(
           utils.namehash(`${entry.name}.${tld}`),
           utils.id(subdomain.name),
           deployer.address,
-          { gasLimit: 1000000 },
         )
 
         await tx.wait()
@@ -176,7 +183,7 @@ async function main() {
 
         // Set resolver for subdomain
         tx = await ENSRegistry.connect(deployer).setResolver(
-          subdomainNamehash,
+          utils.namehash(`${subdomain.name}.${entry.name}.${tld}`),
           publicResolverAddress,
         )
 
@@ -186,7 +193,7 @@ async function main() {
 
         //Set forward record
         tx = await PublicResolver.connect(deployer)['setAddr(bytes32,address)'](
-          subdomainNamehash,
+          utils.namehash(`${subdomain.name}.${entry.name}.${tld}`),
           subdomain.address,
         )
 
--- scripts/addENSNameBatch2.js
+++ scripts/addENSNameBatch2.js
@@ -25,105 +25,117 @@ const registrarAddress = '0x79EAFe3655C0E94b0CE180E76a4bBa4B3ED24555' // dummy a
 const reverseRegistrarAddress = '0x6D33498AF82D2D667e2626fCbbf56685161c2F5a' // dummy address - change to real address
 const publicResolverAddress = '0x1a9522C0C88060cDb59AABA2c52648f19aCc7329' // dummy address - change to real address
 const duration = 86400 * 365
-const labelhash = (label) => utils.keccak256(utils.toUtf8Bytes(label))
-
 const batchData = [
   {
     name: 'dtcc',
     subdomains: [
       {
-        name: 'margincore-ny',
-        address: '0x14D6F2201FC8c8cfd65Dd349dD53d165198C22f7',
+        name: 'new-hello',
+        address: '0xc0Ee98ac1a44B56fbe2669A3B3C006DEB6fDd0f9',
       },
-      {
-        name: 'margincore-tokyo',
-        address: '0x803B307bbEcB8D8E55959C0F21cD6a5E008D58Ef',
-      },
-      {
-        name: 'margincore-paris',
-        address: '0x81f845B586fFf45C68F01D2B4d9c0DB727a8cE3d',
-      },
-      {
-        name: 'bravo-bank',
-        address: '0x88Cf1538EEB86462C2AC66702A80cC6a8EC4fD04',
-      },
-      {
-        name: 'socgen',
-        address: '0x93f0BC752C988aB66DEA14dD8Aa6176b2B511381',
-      },
-      {
-        name: 'wellington',
-        address: '0xaEbA76b426fEE6680872a84942fda1bC4D1A87Ef',
-      },
-      {
-        name: 'bank-of-america',
-        address: '0x6774f079Dd360cf228d09737BC3657B8672bABBB',
-      },
-      {
-        name: 'sterling-bank',
-        address: '0xfABB6B1528B83D91941c3CB58e7954522c24d8CA',
-      },
-      {
-        name: 'fidelity',
-        address: '0xce1279683e3A060Df76E72f0D1717CF95D42Be9b',
-      },
-      {
-        name: 'ficc',
-        address: '0x37a7F8B45f7dCCC4d88eCf8B8866F056a512395F',
-      },
-      {
-        name: 'nscc',
-        address: '0x801839DebB7061406EF738d507D05830F2fF43f1',
-      },
-      {
-        name: 'jscc',
-        address: '0x01d2D3da7a42F64e7Dc6Ae405F169836556adC86',
-      },
-      {
-        name: 'crypto-ex1',
-        address: '0x57122554635EF92adC3c9252Ea996ce9Fb72537C',
-      },
-      {
-        name: 'dtcc-da',
-        address: '0xc31a19c17981b6ACd925Af8c8F5BF7a63aeAA028',
-      },
-      {
-        name: 'dtcc',
-        address: '0x9300787cFe46c0468cca4aa1b439fa627FAa28C7',
-      },
-      {
-        name: 'bny',
-        address: '0xF466381A18b44Ef898B3e95d616A8Cae53a560D0',
-      },
-      {
-        name: 'euroclear',
-        address: '0x1BE6Fe727aCD01f86ab5b5C7b57d42152c4CB771',
-      },
-      {
-        name: 'blackrock',
-        address: '0xAF0AD51DaD551833495cD679344D71fB6990d3a2',
-      },
-      {
-        name: 'crypto-ex2',
-        address: '0xfaB5663a0d7F277F926381B5B5b1C2078288700E',
-      },
-      {
-        name: 'jp-morgan',
-        address: '0x4ea3efe0207a9A208C0da93A3f9A7A898393fe4D',
-      },
-      {
-        name: 'margincore-london',
-        address: '0x9d1aE6d84e3A385e667CeE70d4dFe29Ff0ea7605',
-      },
-      {
-        name: 'yan-asset-manager',
-        address: '0xbB22D30cF7075339FbbC12035AFBd8aa23C7f355',
-      },
+      // {
+      //   name: 'margincore-tokyo',
+      //   address: '0x803B307bbEcB8D8E55959C0F21cD6a5E008D58Ef',
+      // },
+      // {
+      //   name: 'zach',
+      //   address: '0x5eb3Bc0a489C5A8288765d2336659EbCA68FCd00',
+      // },
+      // {
+      //   name: 'margincore-ny',
+      //   address: '0x14D6F2201FC8c8cfd65Dd349dD53d165198C22f7',
+      // },
+      // {
+      //   name: 'margincore-tokyo',
+      //   address: '0x803B307bbEcB8D8E55959C0F21cD6a5E008D58Ef',
+      // },
+      // {
+      //   name: 'margincore-paris',
+      //   address: '0x81f845B586fFf45C68F01D2B4d9c0DB727a8cE3d',
+      // },
+      // {
+      //   name: 'bravo-bank',
+      //   address: '0x88Cf1538EEB86462C2AC66702A80cC6a8EC4fD04',
+      // },
+      // {
+      //   name: 'socgen',
+      //   address: '0x93f0BC752C988aB66DEA14dD8Aa6176b2B511381',
+      // },
+      // {
+      //   name: 'wellington',
+      //   address: '0xaEbA76b426fEE6680872a84942fda1bC4D1A87Ef',
+      // },
+      // {
+      //   name: 'bank-of-america',
+      //   address: '0x6774f079Dd360cf228d09737BC3657B8672bABBB',
+      // },
+      // {
+      //   name: 'sterling-bank',
+      //   address: '0xfABB6B1528B83D91941c3CB58e7954522c24d8CA',
+      // },
+      // {
+      //   name: 'fidelity',
+      //   address: '0xce1279683e3A060Df76E72f0D1717CF95D42Be9b',
+      // },
+      // {
+      //   name: 'ficc',
+      //   address: '0x37a7F8B45f7dCCC4d88eCf8B8866F056a512395F',
+      // },
+      // {
+      //   name: 'nscc',
+      //   address: '0x801839DebB7061406EF738d507D05830F2fF43f1',
+      // },
+      // {
+      //   name: 'jscc',
+      //   address: '0x01d2D3da7a42F64e7Dc6Ae405F169836556adC86',
+      // },
+      // {
+      //   name: 'crypto-ex1',
+      //   address: '0x57122554635EF92adC3c9252Ea996ce9Fb72537C',
+      // },
+      // {
+      //   name: 'dtcc-da',
+      //   address: '0xc31a19c17981b6ACd925Af8c8F5BF7a63aeAA028',
+      // },
+      // {
+      //   name: 'dtcc',
+      //   address: '0x9300787cFe46c0468cca4aa1b439fa627FAa28C7',
+      // },
+      // {
+      //   name: 'bny',
+      //   address: '0xF466381A18b44Ef898B3e95d616A8Cae53a560D0',
+      // },
+      // {
+      //   name: 'euroclear',
+      //   address: '0x1BE6Fe727aCD01f86ab5b5C7b57d42152c4CB771',
+      // },
+      // {
+      //   name: 'blackrock',
+      //   address: '0xAF0AD51DaD551833495cD679344D71fB6990d3a2',
+      // },
+      // {
+      //   name: 'crypto-ex2',
+      //   address: '0xfaB5663a0d7F277F926381B5B5b1C2078288700E',
+      // },
+      // {
+      //   name: 'jp-morgan',
+      //   address: '0x4ea3efe0207a9A208C0da93A3f9A7A898393fe4D',
+      // },
+      // {
+      //   name: 'margincore-london',
+      //   address: '0x9d1aE6d84e3A385e667CeE70d4dFe29Ff0ea7605',
+      // },
+      // {
+      //   name: 'yan-asset-manager',
+      //   address: '0xbB22D30cF7075339FbbC12035AFBd8aa23C7f355',
+      // },
     ],
   },
 ]
 
 async function main() {
+  const [deployer] = await ethers.getSigners()
+
   // Get the ENSRegistry contract
   const ENSRegistry = await ethers.getContractAt(
     'ENSRegistry',
@@ -143,16 +155,37 @@ async function main() {
   )
 
   for (const entry of batchData) {
-    let tx
-    let [deployer] = await ethers.getSigners()
+    let [deployer, ...accounts] = await ethers.getSigners()
 
+    const namehash = utils.namehash(`${entry.name}.${tld}`)
     try {
+      let i = 0
       for (const subdomain of entry.subdomains) {
         const subdomainNamehash = utils.namehash(
           `${subdomain.name}.${entry.name}.${tld}`,
         )
 
+        // assert.equal(
+        //   accounts[i].address,
+        //   subdomain.address,
+        //   'Account and subdomain address should be same',
+        // )
+
         console.log(`Setting subdomain ${subdomain.name}.${entry.name}.${tld}`)
+
+        // tx = await FIFSRegistrar.connect(accounts[i]).register(
+        //   utils.namehash(`${subdomain.name}.${entry.name}`),
+        //   subdomain.address,
+        //   duration,
+        //   { gasLimit: 1000000 },
+        // )
+
+        // await tx.wait()
+
+        const isAvailable = await FIFSRegistrar.available(
+          utils.namehash(`${subdomain.name}.${entry.name}`),
+        )
+        console.log('is available. ...', isAvailable)
 
         console.log(
           `Set subdomain owner for ${subdomain.name}.${entry.name}.${tld}`,
@@ -167,31 +200,25 @@ async function main() {
           { gasLimit: 1000000 },
         )
 
-        await tx.wait()
+        console.log(1)
 
-        console.log(
-          'Set subnode owner for',
-          `${subdomain.name}.${entry.name}.${tld}`,
-        )
-
-        // Set resolver for subdomain
+        // // Set resolver for subdomain
         tx = await ENSRegistry.connect(deployer).setResolver(
           subdomainNamehash,
           publicResolverAddress,
         )
-
         await tx.wait()
 
+        await tx.wait()
         console.log(`Set resolver for ${subdomain.name}.${entry.name}.${tld}`)
 
         //Set forward record
-        tx = await PublicResolver.connect(deployer)['setAddr(bytes32,address)'](
+        tx = await PublicResolver['setAddr(bytes32,address)'](
           subdomainNamehash,
           subdomain.address,
         )
 
         await tx.wait()
-
         console.log(
           `Set forward record for ${subdomain.name}.${entry.name}.${tld} to ${subdomain.address}`,
         )
@@ -222,7 +249,7 @@ async function main() {
         // )
         // await tx.wait();
 
-        tx = await ReverseRegistrar.connect(deployer).setNameForAddr(
+        tx = await ReverseRegistrar.setNameForAddr(
           subdomain.address,
           subdomain.address,
           publicResolverAddress,
@@ -276,6 +303,7 @@ async function main() {
         console.log('')
 
         console.log('Domain Name Registered Is: ', domainName)
+        i++
       }
     } catch (error) {
       console.error(`Error processing ${entry.name}.${tld}:`, error)
